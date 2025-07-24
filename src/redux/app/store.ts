e736858ebb5eae6b732f3fafe511289e21a56cae
AkikozZM <zgeming@seas.upenn.edu>
2025-07-23 18:03:22 -0700

    Set up Redux-persist and authSlice

--- src/redux/app/store.ts
+++ src/redux/app/store.ts
@@ -1,18 +1,49 @@
-// store.ts
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import authenticateReducer from "../features/authenticate/authenticateSlice";
 
-import authenticateReducer from '../features/authenticate/authenticateSlice';
-
-
+// TODO: Combine reducers here:
 const rootReducer = combineReducers({
   authenticate: authenticateReducer,
 });
 
+// Redux Persist config
+const persistConfig = {
+  key: "root", // Key for localStorage
+  storage, // Defaults to localStorage
+  whitelist: ["authenticate"], // Whitelist the slices
+};
+
+// Wrap reducer in persistReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create store
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+
+  // Redux-persist -- Redux Toolkit
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-// inferred TS types for use throughout your app
+// Create persistor
+export const persistor = persistStore(store);
+
+// Export types
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export type AppStore = typeof store;
--- src/redux/features/authenticate/authenticateSlice.ts
+++ src/redux/features/authenticate/authenticateSlice.ts
@@ -5,11 +5,15 @@ import type { RootState } from "../../app/store";
 export interface AuthenticateState {
   isLogin: boolean;
   role: "HR" | "Employee" | "";
-};
+  username: string;
+  id: string;
+}
 
 const initialState: AuthenticateState = {
   isLogin: false,
   role: "",
+  username: "",
+  id: "",
 };
 
 const authenticateSlice = createSlice({
@@ -22,12 +26,21 @@ const authenticateSlice = createSlice({
     setRole(state, action: PayloadAction<AuthenticateState["role"]>) {
       state.role = action.payload;
     },
+    setUsername(state, action: PayloadAction<string>) {
+      state.username = action.payload;
+    },
+    setId(state, action: PayloadAction<string>) {
+      state.id = action.payload;
+    },
   },
 });
 
-export const { setIsLogin, setRole } = authenticateSlice.actions;
+export const { setIsLogin, setRole, setUsername, setId } =
+  authenticateSlice.actions;
 
 export const selectIsLogin = (state: RootState) => state.authenticate.isLogin;
 export const selectRole = (state: RootState) => state.authenticate.role;
+export const selectUsername = (state: RootState) => state.authenticate.username;
+export const selectId = (state: RootState) => state.authenticate.id;
 
 export default authenticateSlice.reducer;
